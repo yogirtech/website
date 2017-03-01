8c87e03ffb8ef8bdac5e45e03174f5c9268aeda1
YogiR Tech <yogirtech@Rajas-MBP.lan>
2017-02-28 19:10:14 -0800

    Initial Website commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# landing_page
+
+# install the dependencies with npm
+$ npm install
+
+# start the server
+$ npm start
+```
+go to [http://localhost:8080](http://localhost:8080) in your browser.
---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+import { ApiService } from './shared';
+
+const $ = require('jquery');
+
+import '../style/app.scss';
+
+@Component({
+  selector: 'my-app', // <my-app></my-app>
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  url = 'https://github.com/preboot/angular2-webpack';
+  title: string;
+
+  constructor(private api: ApiService) {
+    this.title = this.api.title;
+  }
+
+  toggleMobile() {
+    let menu = $('#menu');
+    menu.toggleClass('mobile-menu');
+  }
+}
